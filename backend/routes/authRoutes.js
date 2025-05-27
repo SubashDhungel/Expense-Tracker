bd501eaeb72fd8e2fe7ec2608cd1d440a648d923
Nirajan Dhungel <subashdhungel555@gmail.com>
2025-05-27 12:02:47 +0545

    Backend: Upload Image API Fixed 404 not found issue

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const app= express();
 const  {protect}  = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 const { registerUser, loginUser, getUserInfo } = require('../controllers/authController');
@@ -9,12 +8,12 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/getUserInfo',protect, getUserInfo);
 
-router.post('/upload-image', upload.single('image',(req,res,next)=>{
+router.post('/upload-image', upload.single('image'), (req, res) => {
     if(!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
     const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
     res.status(200).json({ message: 'Image uploaded successfully', imageUrl });
+});
 
-}) );
 module.exports = router;
