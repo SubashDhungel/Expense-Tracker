37e2968428590db5c355861cdfcc8068d1423a4c
Nirajan Dhungel <subashdhungel555@gmail.com>
2025-06-06 16:02:28 +0545

    Minor update

--- frontend/src/context/UserProvider.jsx
+++ frontend/src/context/UserProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { UserContext } from './UserContext';
+import { UserContext } from './userContext';
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
