1da3610a721282b45e157da48c3be1f8d5c2317f
Subash Dhungel <subash@kali.subash.net>
2025-06-08 13:42:14 +0545

    On Linux

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,5 @@ app.use('/api/v1/dashboard',dashboardRoutes);
 app.use('/uploads', express.static(path.join(__dirname,"uploads")));
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
     console.log(`Server is running on port ${PORT}`);
-=======
-    console.log(`Server is running on port ${PORT}`); 
->>>>>>> 880ffab (On Linux)
 });
